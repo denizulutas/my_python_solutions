3f287dd4147e3e81fa93ecdd38033eba1712fff0
Deniz <fulutas4242@gmail.com>
2021-11-07 21:08:44 -0800

    initial commit

---
+++ new.py
@@ -0,0 +1,6 @@
+name = input(" Enter your first name : ")
+in_name =name.strip().lower()
+if in_name =="joseph":
+  print("Hello, Joseph! The password is: W@12")
+else:
+  print("Hello, {}! See you later".format(name))
